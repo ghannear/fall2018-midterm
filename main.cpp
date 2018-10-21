05c84e7c0b1eb23be36b935154e579c4f1b6fb75
ghannea_meg rave <ghannea_megrave@cis.vvc.edu>
2018-10-20 23:06:35 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-/**
-Description:
-Filename:
-Author:
-Date:
+/*
+ * File: main.cpp
+ * Description: Counts the number of times a specific character is in a string that was put in by the   user.
+ * Author: Ghannea Rave    
+ * Email: ghanner1589@student.vvc.edu
+ * Date: October 18, 2018
 */
 
 using namespace std;
